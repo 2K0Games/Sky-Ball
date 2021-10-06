6934790ab37dd3a264f169fc89d47af251c61ec7
2K0 Games <47676827+2K0Games@users.noreply.github.com>
2021-10-06 01:27:13 -0400

    Add files via upload

---
+++ script/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour {
+    public Transform lookAt;
+   
+    private Vector3 desiredPosition;
+    private Vector3 offset;
+
+   
+
+    private float smoothSpeed = 7.5f;
+    private float distance= 10.0f;
+    private float yOffset = 3.5f;
+
+	// Use this for initialization
+	void Start () {
+        offset = new Vector3(0, yOffset, -1f * distance);
+	}
+	
+	// Camera for controller
+    
+}
